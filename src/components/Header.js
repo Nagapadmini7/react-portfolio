bd2103c436000339dc67a173ea374bf399658e95
Nagapadmini7 <96631167+Nagapadmini7@users.noreply.github.com>
2023-06-16 23:18:45 +0530

    completed banner

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const Header = () => {
+  return <header className='bg-pink-300 py-8'>
+   <div className='container mx-auto'>
+    
+      <button className='btn btn-sm'>Work with me</button>
+      </div> </header>;
+};
+
+export default Header;
+ 
