c4a7ccbf5553f89e41a7cfc0487a9d121485768e
Nagapadmini7 <96631167+Nagapadmini7@users.noreply.github.com>
2024-04-13 14:53:57 +0530

    updated the job profile

--- src/components/Services.js
+++ src/components/Services.js
@@ -5,6 +5,7 @@ import Gssoc from '../assets/gssoc.png'
 import Pr from '../assets/pr.png'
 import Eve from '../assets/eve.jpeg'
 import superteam from '../assets/superteam.jpeg'
+import it from '../assets/logo.jpeg'
 
 
 const Services = () => {
@@ -27,12 +28,12 @@ const Services = () => {
     </div>
     <div class="group relative cursor-pointer items-center justify-center overflow-hidden transition-shadow hover:shadow-xl hover:shadow-black/30">
       <div class="h-96 w-72">
-        <img class="h-full w-full object-contain transition-transform duration-500 group-hover:rotate-3 group-hover:scale-125" src={Ssoc} alt="" />
+        <img class="h-full w-full object-contain transition-transform duration-500 group-hover:rotate-3 group-hover:scale-125" src={it} alt="" />
       </div>
       <div class="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-black group-hover:from-black/70 group-hover:via-black/60 group-hover:to-black/70"></div>
       <div class="absolute inset-0 flex translate-y-[60%] flex-col items-center justify-center px-9 text-center transition-all duration-500 group-hover:translate-y-0">
-        <h1 class="font-dmserif text-3xl font-bold text-white">Ssoc</h1>
-        <p class="mb-3 text-lg italic text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100">Open-source Developer</p>
+        <h1 class="font-dmserif text-3xl font-bold text-white">IT Sphere Hub</h1>
+        <p class="mb-3 text-lg italic text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100">Business Growth Specialist</p>
         
       </div>
     </div>
@@ -87,6 +88,23 @@ const Services = () => {
       <div class="absolute inset-0 flex translate-y-[60%] flex-col items-center justify-center px-9 text-center transition-all duration-500 group-hover:translate-y-0">
         <h1 class="font-dmserif text-3xl font-bold text-white">Superteam India</h1>
         <p class="mb-3 text-lg italic text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100">Contributor</p>
+      </div>
+    </div>
+    
+  </div>
+  
+     </div>
+     <div className='  min-h-screen items-center justify-center mt-[-250px] flex flex-col lg:flex-row gap-x-10'>
+     <div class="grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-3">
+    <div class="group relative cursor-pointer items-center justify-center overflow-hidden transition-shadow hover:shadow-xl hover:shadow-black/30">
+      <div class="h-96 w-72">
+        <img class="h-full w-full object-contain transition-transform duration-500 group-hover:rotate-3 group-hover:scale-125" src={Ssoc} alt="" />
+      </div>
+      <div class="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-black group-hover:from-black/70 group-hover:via-black/60 group-hover:to-black/70"></div>
+      <div class="absolute inset-0 flex translate-y-[60%] flex-col items-center justify-center px-9 text-center transition-all duration-500 group-hover:translate-y-0">
+        <h1 class="font-dmserif text-3xl font-bold text-white">Ssoc</h1>
+        <p class="mb-3 text-lg italic text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100">Open-Source Developer</p>
+        
       </div>
     </div>
     
